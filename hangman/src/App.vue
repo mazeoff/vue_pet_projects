184f826fed8fa6051f397c670018e5ac3c853626
Nikonorov Gleb <maze0ff@ya.ru>
2024-06-18 16:53:11 +0300

    Add new project

---
+++ hangman/src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import GameHeader from './components/GameHeader.vue';
+import GameWord from './components/GameWord.vue';
+import GameFigure from './components/GameFigure.vue';
+import GameWrongLetters from './components/GameWrongLetters.vue';
+import GamePopup from './components/GamePopup.vue';
+</script>
+<template>
+	<GameHeader />
+	<div class="game-container">
+		<GameFigure />
+		<GameWrongLetters />
+
+		<GameWord />
+	</div>
+
+	<!-- Container for final message -->
+	<GamePopup />
+
+	<!-- Notification -->
+	<div class="notification-container show">
+		<p>Вы уже вводили этот символ</p>
+	</div>
+</template>
